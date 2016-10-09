a86dd6b5ded3b021426527a7fdcff11ee977f5d0
Roman Kowalski <kowal2499@gmail.com>
2016-10-09 20:11:18 +0200

    nowe: bystrzaki przedszkolaki
    	modified:   database/news.xml
    	new file:   img/bystrzak.jpg
    	modified:   scripts/events.js

--- scripts/events.js
+++ scripts/events.js
@@ -5,14 +5,24 @@ var eventsDatabase = [
                             //        date: "04.10.2016, godz. 16.00",
                             //        action: "<p>Spotkanie organizacyjne dla uczestników <strong>'SZKOŁY DLA RODZICÓW, czyli jak mówić, żeby dzieci nas słuchały i jak słuchać, żeby dzieci do nas mówiły.'</strong> - edycja 2016/2017</p>"
                             // },
+
+
+                           
                             {
                                    date: "13.10.2016 godz. 17:00",
                                    action: "<p>Szkolenie dla logopedów.</p>"
                             },
 
+                            
+
                             {
                                    date: "21.10.2016 godz. 10:30",
                                    action: "<p><a href='subpages/pomocDlaNauczycieli.html#sieć_współpracy'>Sieć współpracy i samokształcenia dla socjoterapeutów.</a></p>"
+                            }, 
+                            
+                            {
+                                   date: "24.10.2016 godz. 15:00",
+                                   action: "<p>'Bystrzaki - przedskolaki' - spotkanie organizacyjne</p>"
                             }
 
                             
